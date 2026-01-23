1d72683c43e09b0258ba419fd2456d62df5e0e10
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-01-23 12:01:42 +0000

    auto gen queries and update variable names for auth because of it

--- backend/internal/auth/auth.py
+++ backend/internal/auth/auth.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 from .password import hash_password
 from pydantic import BaseModel, EmailStr, StringConstraints
-from internal.queries.consumer import GetConsumerRow, Querier as ConsumerQuery
-from internal.queries.seller import CreateSellerParams, GetSellerRow, Querier as SellerQuery
-from internal.queries.models import UsersRole
+from internal.queries.models import Consumer, Seller
+from internal.queries.consumer import Querier as ConsumerQuerier
+from internal.queries.seller import CreateSellerParams, Querier as SellerQuerier
+from internal.queries.models import UserRole
 from internal.queries.user import Querier as UserQuery
 from internal.database import database_manager
 
@@ -13,32 +14,27 @@ class RegisterConsumerForm(BaseModel):
     first_name: Annotated[str, StringConstraints(min_length=1)]
     last_name: Annotated[str, StringConstraints(min_length=1)]
 
-def register_consumer(register_consumer_form: RegisterConsumerForm) -> GetConsumerRow | Exception:
+def register_consumer(register_consumer_form: RegisterConsumerForm) -> Consumer | Exception:
     hash = hash_password(register_consumer_form.password)
     engine = database_manager.get_engine()
     if isinstance(engine, Exception):
         return engine
     with engine.connect() as conn:
-        UserQuery(conn).create_user(
+        user = UserQuery(conn).create_user(
             email=register_consumer_form.email,
             pw_hash=hash,
-            role=UsersRole.CONSUMER
-        )
-        user = UserQuery(conn).get_user_by_email(
-            email = register_consumer_form.email
+            role=UserRole.CONSUMER
         )
         if not user:
             return Exception("Internal user creation error")
-        ConsumerQuery(conn).create_consumer(
+        consumer = ConsumerQuerier(conn).create_consumer(
             user_id=user.user_id,
             fname=register_consumer_form.first_name,
             lname=register_consumer_form.last_name
         )
-        consumer = ConsumerQuery(conn).get_consumer(
-            user_id=user.user_id
-        )
         if not consumer:
             return Exception("Internal consumer creation error")
+        conn.commit()
         return consumer
 
 class RegisterSellerForm(BaseModel):
@@ -52,23 +48,20 @@ class RegisterSellerForm(BaseModel):
     region: str
     country: str
 
-def register_seller(register_seller_form: RegisterSellerForm) -> GetSellerRow | Exception:
+def register_seller(register_seller_form: RegisterSellerForm) -> Seller | Exception:
     hash = hash_password(register_seller_form.password)
     engine = database_manager.get_engine()
     if isinstance(engine, Exception):
         return engine
     with engine.connect() as conn:
-        UserQuery(conn).create_user(
+        user = UserQuery(conn).create_user(
             email=register_seller_form.email,
             pw_hash=hash,
-            role=UsersRole.CONSUMER
-        )
-        user = UserQuery(conn).get_user_by_email(
-            email = register_seller_form.email
+            role=UserRole.CONSUMER
         )
         if not user:
             return Exception("Internal user creation error")
-        SellerQuery(conn).create_seller(CreateSellerParams(
+        seller = SellerQuerier(conn).create_seller(CreateSellerParams(
                 user_id=user.user_id,
                 seller_name=register_seller_form.seller_name,
                 address_line1=register_seller_form.address_line1,
@@ -78,9 +71,7 @@ def register_seller(register_seller_form: RegisterSellerForm) -> GetSellerRow | 
                 region=register_seller_form.region,
                 country=register_seller_form.country
         ))
-        seller = SellerQuery(conn).get_seller(
-            user_id=user.user_id
-        )
         if not seller:
             return Exception("Internal seller creation error")
+        conn.commit()
         return seller
