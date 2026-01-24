ff216e7e10f699e5d3b807c8a6456941560e0008
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-01-24 20:06:53 +0000

    added get session to get role

--- backend/routers/session.py
+++ backend/routers/session.py
@@ -1,20 +1,36 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from internal.auth import create_token, delete_token, basic_auth, bearer_auth
 from internal.database import database_dependency
+from internal.queries.models import UserRole
 from internal.queries.token import GetSessionByTokenRow
 from fastapi import APIRouter, Security
 
 router = APIRouter(prefix="/session", tags=["session"])
 
-class SessionResponseModel(BaseModel):
+class TokenResponseModel(BaseModel):
     token: str
     expires_at: datetime
 
-@router.post("", response_model=SessionResponseModel, status_code=201)
+@router.post("", response_model=TokenResponseModel, status_code=201)
 def create_session(conn: database_dependency, user_id: int = Security(basic_auth)):
     token = create_token(user_id, conn)
-    return SessionResponseModel(token=token.token, expires_at=token.expires_at)
+    return TokenResponseModel(token=token.token, expires_at=token.expires_at)
+
+class SessionResponseModel(BaseModel):
+    email: EmailStr
+    role: UserRole
+    token: str
+    expires_at: datetime
+
+@router.get("", response_model=SessionResponseModel, status_code=200)
+def get_session(session: GetSessionByTokenRow = Security(bearer_auth)):
+    return SessionResponseModel(
+        email = session.email,
+        role = session.role,
+        token = session.token,
+        expires_at = session.expires_at
+    )
 
 @router.delete("", status_code=200)
 def delete_session(conn: database_dependency, session: GetSessionByTokenRow = Security(bearer_auth)):
