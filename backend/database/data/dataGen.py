ab58ef21217dd55d7ce2ce633e264ef5d1d712ab
momopj <70693419+momopj@users.noreply.github.com>
2026-02-09 09:36:18 +0000

    data start

---
+++ backend/database/data/dataGen.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import random
+from faker import Faker
+
+fake =Faker()
+
+no_sellers = 25
+no_consumers = 50
+no_admins = 7
+no_reservations = 400
+no_show_num = 80
+no_expiries = 50
+no_issue_reports = 150
+weeks = 6
