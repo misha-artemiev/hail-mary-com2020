0f16e4edde8f7342bf9750d3ffe8747a34228545
momopj <70693419+momopj@users.noreply.github.com>
2026-02-09 10:50:48 +0000

    profile functions
    sellers, consumer and admin tables made from initale user table -> Specialisation core type shit

--- backend/database/data/dataGen.py
+++ backend/database/data/dataGen.py
@@ -47,3 +47,51 @@ def generate_users():
             'last_login': START_DATE + timedelta(days=random.randint(0, 42))
         })
     return pd.DataFrame(users)
+
+def generate_profiles(users_df):
+    """this function makes the seperate user role tables from the user table returned by generate_users()"""
+    admin_ids = users_df[users_df['role'] == 'admin']['user_id'].tolist() #makes a list of all ids that are admins for verifcation NOT ADMIN TABLE
+    
+    #handling sellers
+    sellers = []
+    seller_ids = users_df[users_df['role'] == 'seller']['user_id'].tolist()#makes a list of all user ids that are sellers
+    for unique_id in seller_ids:
+        sellers.append({
+            'user_id': unique_id,
+            'seller_name': fake.company(),
+            'verified_by': random.choice(admin_ids),
+            'verification_date': START_DATE - timedelta(days=15),
+            'address_line1': fake.street_address(),
+            'city': fake.city(),
+            'post_code': fake.postcode(),
+            'region': fake.county(),
+            'country': 'United Kingdom'
+        })
+        
+    # handling consumers
+    consumers = []
+    consumer_ids = users_df[users_df['role'] == 'consumer']['user_id'].tolist()#getting consumer ids from the users df
+    for unique_id in consumer_ids:
+        consumers.append({
+            'user_id': unique_id,
+            'f_name': fake.first_name(),
+            'l_name': fake.last_name()
+        })
+        
+    #handling admins
+    admins = []
+    admin_names = {"Muhammed": "Panjwani",
+                   "Massimo": "Belmonte",
+                   "Thomas" : "Noakes",
+                   "Noe" : "Bouchard",
+                   "Misha" : "Artemiev",
+                   "Furkan" : "Yalcintepe",
+                   "Ed" : "Ed_lol"} 
+    for unique_id, (first, last) in zip(admin_ids, admin_names.items()):
+        admins.append({
+            'user_id': unique_id,
+            'f_name': first,
+            'l_name': last
+        })
+        
+    return pd.DataFrame(sellers), pd.DataFrame(consumers), pd.DataFrame(admins)
