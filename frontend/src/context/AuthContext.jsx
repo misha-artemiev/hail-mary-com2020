b314eec25e25057b722846f52e3b589ba513daca
tgn203 <tgn203@exeter.ac.uk>
2026-02-11 04:40:58 +0000

    docs: added documentation for AuthProvider

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -8,6 +8,15 @@ import { getAuthToken, clearAuthToken } from "../services/authService";
 
 const AuthContext = createContext(null);
 
+/**
+ * A component that wraps the application and supplies authentication logic.
+ *
+ * @param {object} props
+ * @param {React.ReactNode} props.children
+ *          - The elements to be protected by authentication context
+ *
+ * @returns {React.Element} the context provider component.
+ */
 export function AuthProvider({ children }) {
     // State object: if the user (within context) is authenticated
     const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -23,11 +32,19 @@ export function AuthProvider({ children }) {
         }
     }, []);
 
+    /**
+     * Stores token data in context.
+     *
+     * @param {Object} tokenData the token data to store.
+     */
     const login = (tokenData) => {
         setIsAuthenticated(true);
         setUserRole(tokenData.role);
     };
 
+    /**
+     * Removes token data from context.
+     */
     const logout = () => {
         clearAuthToken();
         setIsAuthenticated(false);
@@ -43,6 +60,16 @@ export function AuthProvider({ children }) {
     );
 }
 
+/**
+ * Custom hook to access authentication context.
+ * Must be used within an AuthProvider element.
+ *
+ * @returns {Object} the authentication context.
+ *
+ * ---
+ * @example
+ * const { isAuthenticated, userRole, login, logout } = useAuth();
+ */
 export function useAuth() {
     const context = useContext(AuthContext);
     if (!context) {
