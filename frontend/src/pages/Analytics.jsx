3331008b43bec24f0d44f6ee1d096a061e5f3fbe
tgn203 <tgn203@exeter.ac.uk>
2026-02-27 02:10:19 +0000

    fix: formatting of Analytics page

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -17,30 +17,30 @@ export default function Analytics() {
     return (
         <div className="max-w-4xl mx-auto p-4 md:p-6">
             <Card>
-            {/* Header */}
-            <h1 className="text-3xl font-bold text-green-700 mb-6 text-center">
-                Analytics
-            </h1>
+                {/* Header */}
+                <h1 className="text-3xl font-bold text-green-700 mb-6 text-center">
+                    Analytics
+                </h1>
 
-            {/* Images */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
-                <img
-                    src={img1}
-                    alt="title"
-                    className="w-full h-full object-cover border"
-                />
-                <img
-                    src={img2}
-                    alt="title"
-                    className="w-full h-full object-cover border"
-                />
-                <img
-                    src={img3}
-                    alt="title"
-                    className="w-full h-full object-cover border"
-                />
-            </div>
-        </Card>
+                {/* Images */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+                    <img
+                        src={img1}
+                        alt="title"
+                        className="w-full h-full object-cover border"
+                    />
+                    <img
+                        src={img2}
+                        alt="title"
+                        className="w-full h-full object-cover border"
+                    />
+                    <img
+                        src={img3}
+                        alt="title"
+                        className="w-full h-full object-cover border"
+                    />
+                </div>
+            </Card>
         </div>
     );
 }
