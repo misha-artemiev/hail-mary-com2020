fe44c31e93da2b55557140c54bf3453aa2236a0e
tgn203 <tgn203@exeter.ac.uk>
2026-02-27 05:39:19 +0000

    change: hide the Footer on mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,7 +34,9 @@ export default function App() {
             </div>
 
             {/* Footer pushed to the bottom */}
-            <Footer />
+            <div className="hidden md:block">
+                <Footer />
+            </div>
         </div>
     );
 }
