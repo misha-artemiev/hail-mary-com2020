fa696daea94067a3ee76bdb7f13ea4ee8d822eac
tgn203 <tgn203@exeter.ac.uk>
2026-03-03 14:01:22 +0000

    change: dropdown animation to About Us dropdown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -97,7 +97,7 @@ export default function Navbar() {
                         </svg>
                     </button>
                     {isAboutDropdownOpen && (
-                        <div className="absolute left-0 mt-2 w-40 bg-white text-black rounded-md shadow-lg py-1 z-50">
+                        <div className="absolute left-0 mt-2 w-40 bg-white text-black rounded-md shadow-lg py-1 z-50 animate-in">
                             <NavLink
                                 to="/aboutus"
                                 className="block px-4 py-2 hover:bg-gray-100"
