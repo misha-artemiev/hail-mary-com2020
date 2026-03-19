752d1554820a06ef549bf0d7c0454cd9ff3e9229
momopj <muhammedpanjwani657@gmail.com>
2026-03-19 02:20:35 +0000

    brain hurt going to sleep :)

--- backend/internal/analytics/forecast_info.py
+++ backend/internal/analytics/forecast_info.py
@@ -17,3 +17,15 @@ import requests
 from internal.queries.models import DayOfWeek, WeatherFlag
 from .forecast import ForecastQuery
  
+_FORECAST_URL: str = "https://api.open-meteo.com/v1/forecast"
+_PAST_URL: str = "https://archive-api.open-meteo.com/v1/archive"
+_FORECAST_PAST_DAYS: int = 7
+
+_TIMEOUT :int = 10 # seconds
+
+_UK_NATIONS : list[str] = ["ENG", "SCT", "WLS", "NIR"]
+
+# Mapping days of week to integers
+_WEEKDAY_TO_ENUM: dict[int, DayOfWeek] = {
+    i: day for i, day in enumerate(DayOfWeek)
+}
