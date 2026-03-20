a853204ac67c4eeeec8600008af5b74a6d67e29c
Misha Artemiev <mykhailo.artemiev@icloud.com>
2026-03-20 13:29:59 +0000

    added check for null coordinates

--- backend/database/queries/seller.sql
+++ backend/database/queries/seller.sql
@@ -19,7 +19,7 @@ RETURNING *;
 SELECT u.user_id, u.username, u.email, s.seller_name, s.address_line1, s.address_line2, s.city, s.post_code, s.region, s.country, s.verified_by, s.verification_date, u.last_login, u.created_at, s.latitude, s.longitude
 FROM sellers s
 INNER JOIN users u ON s.user_id=u.user_id
-WHERE s.latitude < sqlc.arg(lat_max) AND s.latitude > sqlc.arg(lat_min) AND s.longitude < sqlc.arg(lon_max) AND s.longitude > sqlc.arg(lon_min);
+WHERE s.latitude IS NOT NULL AND s.longitude IS NOT NULL AND s.latitude < sqlc.arg(lat_max) AND s.latitude > sqlc.arg(lat_min) AND s.longitude < sqlc.arg(lon_max) AND s.longitude > sqlc.arg(lon_min);
 
 -- name: VerifySeller :one
 UPDATE sellers
